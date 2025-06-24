2230ba7b85a41c1176d59f6d0a695ef750544e77
Jan Schopohl <jan_schopohl@genua.de>
2025-06-24 14:22:06 +0200

    Extend grpcurl_example script

--- talos-discovery-service/grpcurl_example.sh
+++ talos-discovery-service/grpcurl_example.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-grpcurl -plaintext  -import-path proto -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.Hello
+# Client Hello request
+echo "Hello request:"
+grpcurl -plaintext  -import-path proto -d '{"cluster_id": "123abc", "client_version": "2.0"}' \
+  -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.Hello
+
+# AffiliateUpdate request
+echo "AffiliateUpdate request:"
+grpcurl -plaintext  -import-path proto -d '{"cluster_id": "123abc", "affiliate_id": "321", "affiliate_data": "", "affiliate_endpoints": "", "ttl": "90s"}' \
+  -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.AffiliateUpdate
