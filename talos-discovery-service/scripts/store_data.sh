c1bd2687af7206397b3733ee3af39f86884ec067
Jan Schopohl <jan_schopohl@genua.de>
2025-06-26 10:46:59 +0200

    Created test scripts to store data on the discovery server

---
+++ talos-discovery-service/scripts/store_data.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -r -p "Enter some data: " data
+data=$(echo $data | base64)
+echo $data
+
+grpcurl -plaintext  -import-path ../proto -d '{"cluster_id": "this-cluster-stores-data", "client_version": "2.0"}' \
+  -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.Hello
+
+grpcurl -plaintext  -import-path ../proto -d "{\"cluster_id\": \"this-cluster-stores-data\", \"affiliate_id\": \"1\", \"affiliate_data\": \"$data\", \"affiliate_endpoints\": \"\", \"ttl\": \"300s\"}" \
+  -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.AffiliateUpdate
