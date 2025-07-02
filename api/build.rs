099dd41262cba7ee1d586a5cd3c4d9c2efdc7b1a
Tobias Horst <mail@tobiashorst.de>
2025-07-02 14:29:50 +0200

    add discovery-api submodule

--- api/build.rs
+++ api/build.rs
@@ -4,11 +4,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .build_client(true)
         .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
-            &[
-                "proto/v1alpha1/client/affiliate.proto",
-                "proto/v1alpha1/server/cluster.proto",
-            ],
-            &["proto"],
+            &["v1alpha1/client/affiliate.proto", "v1alpha1/server/cluster.proto"],
+            &["discovery-api/api"],
         )?;
     Ok(())
 }
--- scripts/read_data.sh
+++ scripts/read_data.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-grpcurl -plaintext  -import-path ../api/proto -d '{"cluster_id": "this-cluster-stores-data", "client_version": "2.0"}' \
+grpcurl -plaintext  -import-path ../api/discovery-api/api -d '{"cluster_id": "this-cluster-stores-data", "client_version": "2.0"}' \
   -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.Hello
 
-grpcurl -plaintext  -import-path ../api/proto -d '{"cluster_id": "this-cluster-stores-data", "affiliate_id": "reader", "affiliate_data": "", "affiliate_endpoints": "", "ttl": "300s"}' \
+grpcurl -plaintext  -import-path ../api/discovery-api/api -d '{"cluster_id": "this-cluster-stores-data", "affiliate_id": "reader", "affiliate_data": "", "affiliate_endpoints": "", "ttl": "300s"}' \
   -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.AffiliateUpdate
 
-# Watch request
-grpcurl -plaintext  -import-path ../api/proto -d '{"cluster_id": "this-cluster-stores-data"}' \
+grpcurl -plaintext  -import-path ../api/discovery-api/api -d '{"cluster_id": "this-cluster-stores-data"}' \
   -proto v1alpha1/server/cluster.proto localhost:3000 sidero.discovery.server.Cluster.Watch
 
 # The received data can then be decoded with base64 -d
